624d11c183c1e32a56bf8ee6ca78a123609c6f15
Rachael Judy <64846855+rjudy1@users.noreply.github.com>
2022-05-31 10:45:44 -0400

    Update start_motors.py

--- ros2_ws/start_motors.py
+++ ros2_ws/start_motors.py
@@ -1,7 +1,21 @@
+import math
 import serial
+import sys
 import time
 
 SP = serial.Serial('/dev/TEENSY_PORT', 115200)
 SP.write("M,89,89,**".encode())
 print("Enable power to motor controller now.")
-time.sleep(5)
+input('press enter when done')
+time.sleep(1)
+
+if len(sys.argv) > 1:
+  angle = float(sys.argv[1])
+else:
+  angle = int(input("compass reported heading: "))
+
+heading = angle * math.pi / 180
+if angle > 180:
+  heading -= math.pi * 2
+
+print(f"initial_heading: {heading}")
