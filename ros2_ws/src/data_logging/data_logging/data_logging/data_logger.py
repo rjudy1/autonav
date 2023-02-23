eb30a85b473dd09e577f794201710bece321117a
Ren Murakami <rmurakami@cedarville.edu>
2023-02-23 15:22:13 -0500

    data_logger.py update with IMU

--- ros2_ws/src/data_logging/data_logging/data_logging/data_logger.py
+++ ros2_ws/src/data_logging/data_logging/data_logging/data_logger.py
@@ -48,6 +48,9 @@ class Data_Logger(Node):
         #self.x_sub = self.create_subscription(datatype,      "/tf_static",                        self.callback, 10)
         #self.wheel_sub        = self.create_subscription(String,        "/wheel_distance",                   self.wheel_callback, 10)
 
+        #FIXME
+        self.imu_sub = self.create_subscription(ImuData,  "/imu_data", self.imu_callback, 10)
+
         t = str(round(time.time()))
 
         # create & open log files for nodes we've subscribed to
@@ -116,6 +119,12 @@ class Data_Logger(Node):
             self.wheel_logfile = open(name, 'w')
             self.wheel_writer  = csv.writer(self.wheel_logfile)
 
+        #FIXME
+        if hasattr(self, 'imu_sub'):
+            name               = 'log-' + t + '-imu.csv'
+            self.imu_logfile = open(name, 'w')
+            self.image_writer  = csv.writer(self.imu_logfile)
+
     ## Callback Functions
 
     # image_callback function
@@ -148,6 +157,9 @@ class Data_Logger(Node):
         msg = data.data.split(',')
         self.wheel_writer.writerow(np.concatenate(([time.time()], np.array(msg))))
         #self.get_logger().info("logged.")
+
+    def imu_callback(self, ImuData ):
+        self.imu_writer.writerow(np.array([ImuData.abs_x, ImuData.abs_y, ImuData.abs_z, ImuData.euler_x, ImuData.euler_y, ImuData.euler_z, ImuData.quat_w, ImuData.quat_x, ImuData.quat_y, ImuData.quat_z]))
 
     # destructor
     def __del__(self):
@@ -189,6 +201,9 @@ class Data_Logger(Node):
 
         if hasattr(self, 'wheel_sub'):
             self.wheel_logfile.close()
+
+        if hasattr(self, 'imu_sub'):
+            self.imu_logfile.close()
 
 
 def main(args=None):
